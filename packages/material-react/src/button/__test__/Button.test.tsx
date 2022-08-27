46904e4c5a0b5221155f1975c2cdca1adfd7f511
Pan Jie <fenqi@startdt.com>
2022-08-27 17:36:32 +0800

    style: format the files of button component

--- packages/material-react/src/button/__test__/Button.test.tsx
+++ packages/material-react/src/button/__test__/Button.test.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Filled } from '../demos/Button.stories';
+import * as React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Filled } from '../demos/Button.stories'
 
 describe('Button', () => {
   it('renders all kinds of styles', () => {
-    render(<Filled {...Filled.args} />);
-    expect(screen.getAllByText(/enabled/i)).toHaveLength(2);
-    expect(screen.getAllByText(/add/i)).toHaveLength(2);
-  });
-});
+    render(<Filled {...Filled.args} />)
+    expect(screen.getAllByText(/enabled/i)).toHaveLength(2)
+    expect(screen.getAllByText(/add/i)).toHaveLength(2)
+  })
+})
