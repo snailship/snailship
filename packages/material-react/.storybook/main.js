7a554a5ea280bf76a2cef44ee4cd36c44a5fa16c
Jack Pan <i@jackpan.me>
2022-08-27 22:47:43 +0800

    docs: add docs for typography and color

--- packages/material-react/.storybook/main.js
+++ packages/material-react/.storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
